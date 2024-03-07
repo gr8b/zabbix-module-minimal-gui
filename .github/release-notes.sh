88f435e75fe961e42cc28764a28b18fb4a697865
gr8b <grb8.github@gmail.com>
2024-03-07 17:35:05 +0200

    release notes generation fixes

--- .github/release-notes.sh
+++ .github/release-notes.sh
@@ -2,6 +2,7 @@
 
 script_dir=$(dirname "$(realpath "$0")")
 latest_tag=$(git describe --abbrev=0 --tags 2>/dev/null || echo "none")
+base_url="https://github.com/${GITHUB_REPOSITORY}"
 
 if [ "$latest_tag" != "none" ]; then
     commits=$(git log --oneline "$latest_tag"..)
@@ -21,16 +22,16 @@ while IFS= read -r commit; do
         commit_msg="${commit_msg:$colon_pos}"
         commit_msg="${commit_msg#"${commit_msg%%[![:space:]]*}"}"
         commit_msg="${commit_msg%"${commit_msg##*[![:space:]]}"}"
-        commit_msg=$(echo "$commit_msg" | sed -E 's|PR#([0-9]+)|[PR#\1](/'"${GITHUB_REPOSITORY}"'/pull/\1)|g')
+        commit_msg=$(echo "$commit_msg" | sed -E 's|PR#([0-9]+)|[PR#\1]('"$base_url"'/pull/\1)|g')
 
         if ! grep -q "$commit_msg" <<< "$commit_list"; then
-            commit_list+="* [$commit_hash]($commit_hash) $commit_msg"$'\n'
+            commit_list+="* [$commit_hash]($base_url/commit/$commit_hash) $commit_msg"$'\n'
         fi
     fi
 done <<< "$commits"
 
-export version=${latest_tag}
+export version="$latest_tag"
 export name=$(jq -r '.name' manifest.json)
 export changes="$commit_list"
 
-envsubst < "${script_dir}/templates/RELEASE_NOTES.md"
+envsubst < "$script_dir/templates/RELEASE_NOTES.md"
