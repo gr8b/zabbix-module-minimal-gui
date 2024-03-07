fc3923a773c30fb692e745e66f282c5bb84239cf
gr8b <grb8.github@gmail.com>
2024-03-07 17:04:50 +0200

    replace / with | character as sed pattern delimiter

--- .github/release-notes.sh
+++ .github/release-notes.sh
@@ -15,7 +15,7 @@ commit_list=""
 while IFS= read -r commit; do
     commit_hash=$(echo "$commit" | awk '{print $1}')
     commit_msg=$(echo "$commit" | sed 's/^[a-f0-9]\{7\} \(.*\)$/\1/')
-    commit_msg=$(echo "$commit_msg" | sed -E 's/PR#([0-9]+)/[PR#\1](\/'"${GITHUB_REPOSITORY}"'\/pull\/\1)/g')
+    commit_msg=$(echo "$commit_msg" | sed -E 's|PR#([0-9]+)|[PR#\1](/'"${GITHUB_REPOSITORY}"'/pull/\1)|g')
 
     if ! grep -q "$commit_msg" <<< "$commit_list"; then
         commit_list+="* [$commit_hash](/${GITHUB_REPOSITORY}/commit/$commit_hash) $commit_msg"$'\n'
